b279f5a45d4b051d3b0364c53c32f5f7b1fb999c
DanielWar01 <danielalejoguerra@gmail.com>
2024-11-02 17:19:30 -0500

    Información inicial del dataset sobre estructura y tipo de datos

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ from pandasgui import show
 
 dfStudents = pd.read_csv('./StudentPerformanceFactors.csv')
 
-show(dfStudents)
+#Estructura y el resumen de información del DataFrame
+dfStudents.info()
+
+#La clase del objeto o su tipo de dato categórico o numérico
+type(dfStudents)
+
+#Tipo de datos de cada columna
+print(dfStudents.dtypes)
+
+#Obtención de un resumen estadístico de cada columna numérica
+print(dfStudents.describe())
+
+#Mostrar las primeras 15 filas
+print(dfStudents.head(15))
+
+#Mostrar las últimas 15 filas
+print(dfStudents.tail(15))
